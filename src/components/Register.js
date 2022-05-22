97d9ebb3c8972b0df50967ed9e67a1acd98a4d8b
aldimenur <74653357+aldimenur@users.noreply.github.com>
2022-05-22 12:23:27 +0700

    commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,15 +3,21 @@ import * as React from "react";
 
 function Register() {
   return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
+    <div className="absolute bottom-0 left-0 right-0 top-0 flex justify-center items-center bg-orange-500 ">
+      <div className="h-auto w-72 bg-green-900 shadow-xl rounded-xl flex justify-center items-center flex-col">
+        <div className="tittle-registration h-32 bg-yellow-700 w-full rounded-t-xl flex items-center justify-center">
+          TITTLE
+        </div>
+        <div className="h-40 w-full bg-blue-900 flex justify-center items-center">
+          FORM
+        </div>
+        <div className="h-20 w-full bg-red-900 justify-center items-center flex rounded-b-xl">
+          <nav>
+            <Link to="/"> BACK BUTTON</Link>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
 }
 
